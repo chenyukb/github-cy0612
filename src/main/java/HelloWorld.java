c6641ee3c55b91488063d648d27cd7fb8804eea2
chenyukb <92746141+chenyukb@users.noreply.github.com>
2024-06-12 11:19:18 +0800

    Update HelloWorld.java
    提交试一试

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,7 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello World!");
     System.out.println("wocao World!");
+    System.out.println("wocao World!");
     Map<String, String> envVars = System.getenv();
     for (Map.Entry<String, String> entry : envVars.entrySet()) {
         String key = entry.getKey();
